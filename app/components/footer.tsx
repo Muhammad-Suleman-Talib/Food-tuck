d644cfe33125e0ca0e2a97d70dc0f6865e54964b
Muhammad Suleman <161708844+Muhammad-Suleman-Talib@users.noreply.github.com>
2024-12-10 10:04:31 +0500

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -67,13 +67,11 @@ const Footera: NextPage = () => {
 
       {/* Footer Bottom */}
       <div className="text-center mt-16 text-sm">
-<<<<<<< HEAD
-      <p>Copyright © 2022 by Muhammad Suleman . All Rights Reserved.</p>
-=======
+
       <div className="text-center mt-16 text-sm">
       <p>Copyright © 2022 by Muhammad Suleman . All Rights Reserved.</p>
       </div>
->>>>>>> 724076346199580e2828edbf81c9f2cc7065efb3
+
       </div>
     </div>
   </>
